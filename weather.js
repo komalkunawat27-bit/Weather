d655da00c3161c79f4057c48ed38b2d509c270b3
komalkunawat27-bit <komalkunawat27@gmail.com>
2026-09-19 21:06:14 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,4 +1,4 @@
-```javascript
+
 const cityInput = document.querySelector("#city");
 const result = document.querySelector("#btn");
 const temprature = document.querySelector(".temp");
@@ -133,4 +133,4 @@ result.addEventListener("click", () => {
                 error.message;
         });
 });
-```
+
